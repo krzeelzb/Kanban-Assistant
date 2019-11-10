23570775f77a365429fe59453fd63ffc24486c8b
krzeelzb <krzeczekelzbieta@gmail.com>
2019-11-10 21:43:46 +0100

    options not working

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {
-    dialogflow, Permission,
-    Suggestions, Table, List, Image, BrowseCarouselItem, BrowseCarousel
+    dialogflow, Suggestions, Table, List, Image, BrowseCarouselItem, BrowseCarousel
 } = require('actions-on-google');
 
 const functions = require('firebase-functions');
@@ -15,7 +14,100 @@ const app = dialogflow({debug: false});
 
 const axios = require('axios');
 
+
+app.intent('Default Welcome Intent', (conv) => {
+    conv.ask('Welcome to Kanban Board Assistant');
+    if (!conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT')) {
+
+        return;
+    }
+
+    conv.ask(new List({
+
+        items: {
+            // Add the first item to the list
+            'BOARD': {
+                synonyms: [
+                    'Show me my board',
+                    'Tell me my Kanban Board',
+                    'Show me my Kanban Board',
+                                   ],
+                title: 'Show me my Kanban Board',
+                description: 'See how your Kanban Board looks like',
+                // image: new Image({
+                //     url: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiRspG0pZHiAhWr-yoKHcdIA3QQjRx6BAgBEAU&url=https%3A%2F%2Fstat.gov.pl%2Fpodstawowe-dane%2F&psig=AOvVaw1tJjUu_Ts446LvvrbuYC_7&ust=1557589019220446',
+                //     alt: 'populacja',
+                // }),
+            },
+            'createNewCard': {
+                synonyms: [
+                    'Add new task',
+                    'Make a new card',
+                    'Create a new card named',
+                ],
+                title: 'New Card',
+                description: 'Do you have anythings else new to add?',
+                // image: new Image({
+                //     url: 'https://www.flaticon.com/authors/freepik',
+                //     alt: 'pracujący',
+                // }),
+            },
+            // Add the third item to the list
+            // 'WIEK': {
+            //     synonyms: [
+            //         'sprawdź wiek',
+            //         'Powiedz mi coś o wieku ludzi',
+            //         'Chcę dowiedzieć się czegoś o wieku',
+            //         'Poprosiłbym o dane dotyczące wieku',
+            //         'Dane o wieku',
+            //         'Sprawdź dla mnie osoby w przedziale wiekowym'
+            //
+            //     ],
+            //     title: 'Dane dotyczące wieku',
+            //     description: 'Sprawdź liczbę osób w konkretym przedziale wiekowym w Twojej okolicy lub w wybranym przez Ciebie adresie',
+            //     // image: new Image({
+            //     //     url: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjhmtevqJHiAhUox4sKHcNJAEsQjRx6BAgBEAU&url=https%3A%2F%2Fwww.casecured.com%2Fhome%2F2018%2F2%2F9%2Fage-is-relative&psig=AOvVaw20EYAAeX5-8rC6rN3hwGAK&ust=1557589815375614',
+            //     //     alt: 'wiek',
+            //     // }),
+            // },
+        },
+    }));
+});
+
+const SELECTED_ITEM_CONTEXTS = {
+    'BOARD': 'board',
+    'createNewCard': 'createnewcard',
+
+};
+
+const SELECTED_ITEM_RESPONSES = {
+    'BOARD': 'Here is your board',
+    'createNewCard': 'What is it exactly?',
+};
+
+const SELECTED_ITEM_SUGGESTIONS = {
+    'BOARD': ['ul. Piękna 8, Warszawa', 'Moja lokalizacja'],
+    'createNewCard': ['Unit Testing', 'Peer tesing', 'Code review'],
+};
+
+app.intent('actions.intent.OPTION', (conv, params, option) => {
+    if (option && SELECTED_ITEM_RESPONSES.hasOwnProperty(option)) {
+
+        console.log("ooooooooooooooooooooooooooooo")
+        console.log(option)
+        console.log(conv.contexts);
+        conv.contexts.set(SELECTED_ITEM_CONTEXTS[option], 2);
+        console.log("ccccccccccccccccccccccccccccccc");
+        console.log(conv.contexts);
+    }
+    conv.ask(`${SELECTED_ITEM_RESPONSES[option]}`);
+    // conv.ask(new Suggestions(SELECTED_ITEM_SUGGESTIONS[option]));
+
+});
 app.intent('createNewCard', async (conv, params) => {
+
+    console.log(conv.contexts)
+    console.log("I AM HEERE")
     let taskName = params.taskName;
     console.log(params);
 
@@ -33,6 +125,7 @@ app.intent('createNewCard', async (conv, params) => {
 });
 
 app.intent('deleteCard', async (conv, params) => {
+    console.log("delete")
     let taskToDelete = params.taskName;
     console.log(params);
     return axios.delete("http://localhost:5000/api/cards/delete", {
@@ -48,6 +141,7 @@ app.intent('deleteCard', async (conv, params) => {
 });
 
 app.intent('moveCard', async (conv, params) => {
+    console.log("move")
     let taskToMove = params.taskName;
     let toColumn = params.columnNameTo;
     let fromColumn = params.columnNameFrom;
@@ -67,6 +161,7 @@ app.intent('moveCard', async (conv, params) => {
 
 //TODO: ładniejsza lista zadan do wykoniania
 app.intent('getCardsFromColumn', async (conv, params) => {
+    console.log("cardsform column")
     let fromColumn = params.columnName;
     console.log(params);
     return await axios.post("http://localhost:5000/api/columns/fetchColumnById", {
@@ -80,11 +175,11 @@ app.intent('getCardsFromColumn', async (conv, params) => {
         console.log(e);
         conv.ask("error");
     })
-
 });
 
-//TODO: naprawic zeby sie wyswietlało jak potrzeba, jak est wiecej w jakim zedzie to było wiece
 app.intent('board', async (conv, params) => {
+    console.log(conv.contexts)
+    console.log("board")
     return await axios.get("http://localhost:5000/api/columns/all")
         .then((res) => {
             const columns = res.data.columns;
@@ -110,18 +205,14 @@ app.intent('board', async (conv, params) => {
             console.log(e);
             conv.ask("error");
         })
-
 });
 
 function transpose(original) {
     var copy = [];
     for (var i = 0; i < original.length; ++i) {
         for (var j = 0; j < original[i].length; ++j) {
-            // skip undefined values to preserve sparse array
             if (original[i][j] === undefined) continue;
-            // create row if it doesn't exist yet
             if (copy[j] === undefined) copy[j] = [];
-            // swap the x and y coords for the copy
             copy[j][i] = original[i][j];
         }
     }
